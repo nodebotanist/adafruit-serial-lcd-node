3681aae851305d889d4017a7290da2748bc7da02
Kas Perch <the@nodebotani.st>
2017-08-25 12:26:08 -0500

    Removed sinon

--- test/constructor.test.js
+++ test/constructor.test.js
@@ -1,11 +1,6 @@
 const SerialLCD = require('../seriallcd')
 
-const sinon = require('sinon')
-const SerialPort = require('serialport')
-
-let stub = sinon.createStubInstance(SerialPort);
-
-test('reject invalid baud values', () => {
+test('reject invalid baud values', () => {  
   expect(() => { new SerialLCD({ baud: 4, port: '/dev/tty.usbmodem***' }) }).toThrow('Invalid baud rate for serial LCD. Valid values between 1200-115200 accepted')
 })
 
@@ -16,5 +11,3 @@ test('accept valid baud value', () => {
 test('Error if no port given', () => {
   expect(() => { new SerialLCD({ baud: 115200 }) }).toThrow('Port is required in options object')
 })
-
-// SerialPort.restore()
